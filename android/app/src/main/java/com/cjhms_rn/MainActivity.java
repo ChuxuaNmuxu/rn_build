e68b79a5edc7f8a7ddf0520470ab9b58df54f531
wanjun <243958407@qq.com>
2018-08-09 16:34:29 +0800

    fix：可以隐藏教师端状态栏，但是不能隐藏虚拟按键

--- android/app/src/main/java/com/cjhms_rn/MainActivity.java
+++ android/app/src/main/java/com/cjhms_rn/MainActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
 import android.app.mia.MiaMdmPolicyManager;
 
+/** */
+import android.content.Context;
+import android.view.WindowManager;
+import android.content.pm.ActivityInfo;
+import android.view.View;
+/** */
+
 public class MainActivity extends ReactActivity {
 
     /**联想TAB3定制机修改*/
     MiaMdmPolicyManager miaMdmPolicyManager;
     /** ELink 爱立顺定制机修改*/
-    private elinktek.common.Utils alsUtils;
+    //private elinktek.common.Utils alsUtils;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -23,8 +30,8 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        initSet();
         SplashScreen.show(this, true);
+        initSet();
         super.onCreate(savedInstanceState);
     }
 
@@ -42,11 +49,11 @@ public class MainActivity extends ReactActivity {
             miaMdmPolicyManager.setControlPowerLong(false);
         }
         // ELink 爱立顺定制机修改
-        if (MainApplication.isELinkH8()) {
-            // 使电源键无效
-            alsUtils.disableShortPressPower();
-            alsUtils.disableLongPressPower();
-        }
+        // if (MainApplication.isELinkH8()) {
+        //     // 使电源键无效
+        //     alsUtils.disableShortPressPower();
+        //     alsUtils.disableLongPressPower();
+        // }
     }
 
 
@@ -54,7 +61,7 @@ public class MainActivity extends ReactActivity {
         getWindow().addFlags(WindowManager.LayoutParams.
                 FLAG_KEEP_SCREEN_ON);   //应用运行时，保持屏幕高亮，不锁屏
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 强制横屏
         //隐藏虚拟按键
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -68,44 +75,44 @@ public class MainActivity extends ReactActivity {
 
     public void enableSystemBar(Context context, boolean enable) {
         // 使能状态栏及导航栏
-        if (App.isLenovoTab3()) {
+        if (MainApplication.isLenovoTab3()) {
             if (miaMdmPolicyManager == null) {
-                miaMdmPolicyManager = new MiaMdmPolicyManager(App.getContext());
+                miaMdmPolicyManager = new MiaMdmPolicyManager(MainActivity.this);
             }
             miaMdmPolicyManager.setNavigaBar(!enable);
             miaMdmPolicyManager.setStatusBar(!enable);
         }
-        // ELink 爱立顺定制机修改
-        if (App.isELinkH8()) {
-            // ELink 爱立顺定制机修改
-            if (alsUtils == null) {
-                alsUtils = new elinktek.common.Utils(App.getContext());
-            }
-            if (enable) {
-                alsUtils.enableNavigation();
-            } else {
-                alsUtils.disableNavigation();
-            }
-            alsUtils.disableStatusbar();
-        }
+        // // ELink 爱立顺定制机修改
+        // if (MainApplication.isELinkH8()) {
+        //     // ELink 爱立顺定制机修改
+        //     if (alsUtils == null) {
+        //         alsUtils = new elinktek.common.Utils(MainActivity.this);
+        //     }
+        //     if (enable) {
+        //         alsUtils.enableNavigation();
+        //     } else {
+        //         alsUtils.disableNavigation();
+        //     }
+        //     alsUtils.disableStatusbar();
+        // }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // 重新开启电源键功能
-        if (App.isLenovoTab3()) {
+        if (MainApplication.isLenovoTab3()) {
             //允许短按
             miaMdmPolicyManager.setControlPower(true);
             //允许长按
             miaMdmPolicyManager.setControlPowerLong(true);
         }
-        // ELink 爱立顺定制机修改
-        if (App.isELinkH8()) {
-            alsUtils.enableShortPressPower();  //允许短按
-            alsUtils.enableLongPressPower(); //允许长按
+        // // ELink 爱立顺定制机修改
+        // if (MainApplication.isELinkH8()) {
+        //     alsUtils.enableShortPressPower();  //允许短按
+        //     alsUtils.enableLongPressPower(); //允许长按
 
-        }
+        // }
         enableSystemBar(this,true);
     }
 
