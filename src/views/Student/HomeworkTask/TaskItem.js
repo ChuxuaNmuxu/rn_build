78d42d7bd0ee2d093e7035eed55bb64fd09ae6f6
Mar-er <1051221329@qq.com>
2018-09-13 17:15:00 +0800

    fix：将待计划任务列表的滚动组件由ScrollView改为FlatList并添加让指定任务居中功能

--- src/views/Student/HomeworkTask/TaskItem.js
+++ src/views/Student/HomeworkTask/TaskItem.js
@@ -3,46 +3,9 @@ import {
   View,
   Text,
   TouchableNativeFeedback,
-  StyleSheet,
 } from 'react-native';
 import CIcon from '../../../components/Icon';
-
-const Styles = StyleSheet.create({
-  task: {
-    alignItems: 'center',
-    padding: 24,
-    marginLeft: 24,
-    width: 450,
-    height: 168,
-    backgroundColor: '#54cc82',
-    borderRadius: 5,
-  },
-  icon_box: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 24,
-    height: 96,
-    width: 96,
-    backgroundColor: '#fff',
-    borderRadius: 48,
-  },
-  icon: {
-    fontSize: 60,
-    color: '#54cc82',
-  },
-  flex_row: {
-    flexDirection: 'row',
-  },
-  subject: {
-    fontSize: 24,
-    color: '#fff',
-    width: 450 - 96 - 24 * 3,
-  },
-  details: {
-    fontSize: 18,
-    color: '#fff',
-  },
-});
+import styles from './taskItem.scss';
 
 class TaskItem extends React.Component {
   openTaskDetail = () => {
@@ -62,16 +25,16 @@ class TaskItem extends React.Component {
       <TouchableNativeFeedback
         onPress={this.openTaskDetail}
       >
-        <View style={[Styles.flex_row, Styles.task]}>
-          <View style={Styles.icon_box}>
-            <CIcon style={Styles.icon} name="wendang1" size={25} />
+        <View style={[styles.task]}>
+          <View style={styles.icon_box}>
+            <CIcon style={styles.icon} name="wendang1" size={25} />
           </View>
           <View>
-            <Text style={[Styles.subject]} ellipsizeMode="tail" numberOfLines={1}>
+            <Text style={[styles.subject]} ellipsizeMode="tail" numberOfLines={1}>
               6-22 语文作业6-22 语文作业6-22 语文作业6-22 语文作业6-22 语文作业
             </Text>
-            <Text style={Styles.details}>预计耗时：15′</Text>
-            <Text style={Styles.details}>截止提交时间：6-24 24:00</Text>
+            <Text style={styles.details}>预计耗时：15′</Text>
+            <Text style={styles.details}>截止提交时间：6-24 24:00</Text>
           </View>
         </View>
       </TouchableNativeFeedback>
---
+++ src/views/Student/HomeworkTask/TaskList.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import TaskItem from './TaskItem';
+import styles from './taskList.scss';
+
+class TaskList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flatlistWidth: 0,
+    };
+    this.flatList = null;
+  }
+
+  componentDidMount() {
+    /**
+     * 必须为异步时才能起作用，FlatList默认从index为0时开始加载。
+     * 当使用scrollToIndex时需要先将对应的元素加载出来,然后才能让指定元素居中
+     */
+    const wait = new Promise(resolve => setTimeout(resolve, 500));
+    wait.then(() => {
+      // 将位于指定位置的元素滚动到可视区的指定位置，当viewPosition 为 0 时将它滚动到屏幕顶部，为 1 时将它滚动到屏幕底部，为 0.5 时将它滚动到屏幕中央。
+      this.flatList.scrollToIndex({
+        animated: true,
+        index: 10,
+        viewOffset: 0,
+        viewPosition: 0.5,
+      });
+    });
+  }
+
+  onLayout = (e) => {
+    const {
+      layout: {
+        width,
+      },
+    } = e.nativeEvent;
+    this.setState({ flatlistWidth: width });
+  }
+
+  getItemLayout = (data, index) => {
+    const length = 450 + 24;
+    return {
+      length,
+      offset: index * length,
+      index,
+    };
+  }
+
+  keyExtractor = item => item.toString()
+
+  renderItem = item => <TaskItem item={item} />
+
+  renderListEmpty = () => {
+    const {
+      flatlistWidth,
+    } = this.state;
+    // 暂无作业任务
+    return (
+      <View style={[styles.empty, { width: flatlistWidth }]}>
+        <Text style={styles.empty_text}>所有任务已经安排完毕，快去完成它们吧……</Text>
+      </View>
+    );
+  }
+
+  render() {
+    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 34];
+    return (
+      <View
+        style={styles.task_list_box}
+        onLayout={this.onLayout}
+      >
+        <FlatList
+          horizontal
+          ref={(ref) => { this.flatList = ref; }}
+          data={data}
+          style={styles.latList}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+          getItemLayout={this.getItemLayout}
+          // initialScrollIndex={10}
+          ListEmptyComponent={this.renderListEmpty}
+        />
+      </View>
+    );
+  }
+}
+
+export default TaskList;
