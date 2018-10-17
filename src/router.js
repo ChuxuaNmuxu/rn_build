dc267034260c431a333c92dc5b3049958a93a88e
Mar-er <1051221329@qq.com>
2018-10-17 10:54:11 +0800

    fix：修改打包命令修复打包出现的bug

--- src/router.js
+++ src/router.js
@@ -103,7 +103,6 @@ const RouteMap = props => (
           </Stack>
           {/* 错题本 */}
           <Stack
-            initial
             title="wrongNotes" // 配好在zh.js、en.js那边了
             key="ProblemOverviewStack"
             image="cuotiben1"
