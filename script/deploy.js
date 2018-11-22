c26173519af1817f41cdd5a08d5081374e28ba9b
sunb001 <2267660581@qq.com>
2018-11-22 18:18:54 +0800

    e

---
+++ script/deploy.js
@@ -0,0 +1,15 @@
+const shelljs = require('shelljs');
+
+const exec = shelljs.exec;
+
+exec('cd ..');
+
+exec('npm i')
+
+exec('npm run build');
+
+console.log('build success');
+
+exec('chmod +x ./android/gradlew')
+
+exec('./android/gradlew clean aR -b ./android/app/build.gradle')
